7a7739f528e144f24982ce956b1fffe225eb21f5
PaolaSar-art <paola.balderrama@alteadesarrollos.com>
2025-07-05 08:32:04 -0600

    subir

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-import pandas
+import streamlit as st
+
+# Título de la app
+st.title("👋 Bienvenido a mi app Streamlit")
+
+# Entrada de texto
+nombre = st.text_input("¿Cuál es tu nombre?")
+
+# Mostrar saludo si se ingresa un nombre
+if nombre:
+    st.write(f"¡Hola, {nombre}!")
